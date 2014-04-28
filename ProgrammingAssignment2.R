bde0f98e31c55b9d4293e9bf8d3b17ac7eed3464
teffo-atb <teffo22@gmail.com>
2014-04-27 22:49:28 -0400

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -2,12 +2,14 @@
 library(MASS) # for inversing Matrix
 
 # Note: x must be a invertible matrix
-
+# This function takes matrix x as an argument and inverted the matrix x using ginv() function 
 makeCacheMatrix <- function(x){
   y <- ginv(x)
   get <- function() y
 }
 
+# This function takes return value from the above function, inverted it again and return a matrix. 
+# The output value of this function is the original matrix x
 cachemean <- function() {
   inv <- get()
   x <- ginv(inv)
